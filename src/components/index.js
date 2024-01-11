3850e9002071e366990110bcf7d24fb3133b387e
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-11 10:25:54 +0530

    chore: index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
 import Navbar from "./navbar/Navbar";
+import Card from "./card/Card";
+import DetailModal from "./modal/DetailModal";
+import PrimaryBtn from "./primaryBtn/PrimaryBtn";
+import SecondaryBtn from "./secondaryBtn/SecondaryBtn";
+import ModalProvider from "./modalProvider/ModalProvider";
+import CartModal from "./modal/CartModal";
+import Loader from "./loader/Loader";
 
-export { Navbar };
+export { Navbar, Card, DetailModal, PrimaryBtn, SecondaryBtn, ModalProvider, CartModal, Loader };
